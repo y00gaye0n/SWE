ec1c564165db1286d3b30c4ee1b05eeb9c60ae1d
eunly00 <113335538+eunly00@users.noreply.github.com>
2023-11-15 00:53:13 +0900

    Update cal_error_es.py
    새로운 코드 반영 밑 주석추가

--- cal_error_es.py
+++ cal_error_es.py
@@ -1,35 +1,28 @@
 
-error = 0
+error = 0  
 try:
-    x = int(input())
+    x = int(input()) #정수로 입력 받음 정수가 아닐 시 error 출력 
 except ValueError:
     x = 0
     error += 1
 
-op = input()
+op = input() #op는 연산자
 
-while (op != '='):
-    try:
-        y = int(input())
-    except ValueError:
-        y = 0
-        error += 1
+while(op!='='): #연산자가 =이 나오기전까지 반복연산
+    y=int(input()) 
 
-    if (op == '+'):
-        x += y
-    elif (op == '-'):
-        x -= y
-    elif (op == '*'):
-        x *= y
+    if (op == '+'): 
+        x += y 
+    elif (op == '-'): 
+        x -= y 
+    elif (op == '*'): 
+        x *= y 
+    op = input() 
+    
+print(x) #출력
 
-    op2 = input()
-    if (op != op2):
-        op = op2
-        if (op != '='):
-            error += 1
 
 if (error > 0):
     print("ERROR!")
 else:
     print(x)
-

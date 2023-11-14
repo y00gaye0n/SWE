98541ffecf16af1542ef9b252ece679249b6f519
허민 <128207058+i-mymeminn@users.noreply.github.com>
2023-11-15 00:26:57 +0900

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,26 @@
-x=int(input())
+egg(x):
+    if(x=7):
+        print("행운")
+    elif(x=1221):
+        print("종강종강 돌을 던지자")
+    elif(x=18):
+        print("바른말고운말")
+    elif(x=0):
+        print("0은 여기서 False란다")
+    elif(x=1):
+        print("1은 여기서 True란다")
+    elif(x=2):
+        print("Binary..?")
+    elif(x=534):
+        print("컴공생들의 무덤")
 
+x=int(input())
+egg(x)
 op = input()
 
 while(op!='='):
     y=int(input())
-
+    egg(y)
     if (op == '+'):
         x += y
     elif (op == '-'):
